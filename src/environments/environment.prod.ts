510c1c727984679adc2137c068bfa5cf239b9760
Rodrigo A <linelsk@hotmail.com>
2023-07-14 09:40:52 -0600

    enviroment update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,10 +3,10 @@ export const environment = {
   URL_EXPORT: 'https://my.premierds.net/demo/',
 
   
-  API_URL: "https://tecnicah.site/back/violet/api/",
+  //API_URL: "https://tecnicah.site/back/violet/api/",
 
 
-  // API_URL: "https://my.premierds.net/api-test-premier/api/",
+  API_URL: "https://my.premierds.net/api-test-premier/api/",
 
 
   API_URL_na: 'https://my.premierds.net/api-test-premier/',
